469621b560b7e754f671839fb939b8bc868777d5
abobi <dekimiq@gmail.com>
2024-07-09 14:12:14 +0300

    set up project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/competitions',
+        permanent: false,
+      },
+    ];
+  },
+};
+
 
 export default nextConfig;
