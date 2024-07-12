22d59e2e323eafede339c208e38897db0d4b5088
abobi <dekimiq@gmail.com>
2024-07-12 23:59:42 +0300

    added matches on store

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -8,7 +8,7 @@ interface Area {
 interface Competition {
   id: number;
   name: string;
-  emblemUrl: string | null;
+  emblem: string | null;
   area: Area;
 }
 
@@ -17,18 +17,53 @@ interface Team {
   name: string;
 }
 
+interface Score {
+  fullTime: {
+    home: number | null;
+    away: number | null;
+  };
+  halfTime: {
+    home: number | null;
+    away: number | null;
+  };
+}
+
+interface Match {
+  utcDate: string;
+  status:
+    | "SCHEDULED"
+    | "LIVE"
+    | "IN_PLAY"
+    | "PAUSED"
+    | "FINISHED"
+    | "POSTPONED"
+    | "SUSPENDED"
+    | "CANCELED";
+  homeTeam: {
+    name: string;
+  };
+  awayTeam: {
+    name: string;
+  };
+  score: Score;
+}
+
 interface StoreState {
   competitions: Competition[];
   teams: Team[];
+  matches: Match[];
   setCompetitions: (competitions: Competition[]) => void;
   setTeams: (teams: Team[]) => void;
+  setMatches: (matches: Match[]) => void;
 }
 
 const useStore = create<StoreState>((set) => ({
   competitions: [],
   teams: [],
+  matches: [],
   setCompetitions: (competitions) => set({ competitions }),
   setTeams: (teams) => set({ teams }),
+  setMatches: (matches) => set({ matches }),
 }));
 
 export default useStore;
