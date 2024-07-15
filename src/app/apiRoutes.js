c585f98d7adcffeab96072676ebde0a75521fabe
abobi <dekimiq@gmail.com>
2024-07-15 15:33:42 +0300

    replaced http protocol on https

--- src/app/apiRoutes.js
+++ src/app/apiRoutes.js
@@ -9,15 +9,15 @@ const api = axios.create({
 });
 
 const routes = {
-  competitions: () => "http://api.football-data.org/v4/competitions/",
+  competitions: () => "https://api.football-data.org/v4/competitions/",
   competitionIdMatches: (id) =>
-    `http://api.football-data.org/v4/competitions/${id}/matches`,
+    `https://api.football-data.org/v4/competitions/${id}/matches`,
   competitionIdDate: (id, dateFrom, dateTo) =>
-    `http://api.football-data.org/v4/competitions/${id}/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`,
+    `https://api.football-data.org/v4/competitions/${id}/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`,
   commands: () => "https://api.football-data.org/v4/teams",
   commandId: (id) => `https://api.football-data.org/v4/teams/${id}`,
   commandIdMatches: (id) =>
-    `http://api.football-data.org/v4/teams/${id}/matches`,
+    `https://api.football-data.org/v4/teams/${id}/matches`,
   commandIdDate: (id, dateFrom, dateTo) =>
     `https://api.football-data.org/v4/teams/${id}/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`,
 };
