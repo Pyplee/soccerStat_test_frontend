5e0dcf2f9c4661068b2ba044ca8795f54b3a285e
abobi <dekimiq@gmail.com>
2024-08-16 00:42:37 +0300

    fixed error with any type on paginate

--- src/scripts/pagination.ts
+++ src/scripts/pagination.ts
@@ -1,10 +1,12 @@
+import { IMatch } from '../interfaces/IMatch';
+
 const itemsPerPage = 10;
 
-const getTotalPages = (filteredArr) => {
+const getTotalPages = (filteredArr: IMatch[]) => {
   return Math.ceil(filteredArr.length / itemsPerPage);
 };
 
-const paginate = (array, pageNumber) => {
+const paginate = (array: IMatch[], pageNumber: number) => {
   const start = (pageNumber - 1) * itemsPerPage;
   return array.slice(start, start + itemsPerPage);
 };
