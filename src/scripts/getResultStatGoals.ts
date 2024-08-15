61d60c47d3913bc3bc4f5e57cbdea66bcc4a04fa
abobi <dekimiq@gmail.com>
2024-08-15 23:29:41 +0300

    renamed files

--- src/scripts/getResultStatGoals.ts
+++ src/scripts/getResultStatGoals.ts
@@ -1,9 +1,14 @@
-export default function getResultStatGoals(x, y, z, g) {
+export default function getResultStatGoals(
+  x: number | null,
+  y: number | null,
+  z: number | null,
+  g: number | null,
+) {
   if (x !== null && y !== null) {
     if (z !== null && g !== null) {
       return `${x}-${y} (${z}:${g})`;
     }
     return `${x}:${y}`;
   }
-  return 'Нет результата';
+  return null;
 }
